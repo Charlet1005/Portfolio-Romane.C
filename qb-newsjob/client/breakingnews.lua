c85071d73fb1630c17443eaefbbcf9cc9947e514
Charlet1005 <165010033+Charlet1005@users.noreply.github.com>
2025-07-04 16:37:13 +0200

    Delete qb-newsjob directory

--- qb-newsjob/client/breakingnews.lua
+++
@@ -1,8 +0,0 @@
-QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('qb-newsjob:client:ShowBreakingNews', function(text)
-    SendNUIMessage({
-        action = "showBreakingNews",
-        text = text
-    })
-end)
